3a2a0b4861190662afbf972a5ff356d1a0ddc252
zanatooo <m8elzanaty@gmail.com>
2022-03-28 18:50:26 -0700

    <updating the READ ME file>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # zanaty-password-generator
+I created an app to randomly create a new, secure password
+WHEN I click the button, a new passwork is generated after I am presented with a series of prompts for password criteria
+I'll be prompted for password criteria then I'll select which criteria to include in the password
+I'm also prompted for the length of the password then I'll be able to choose a length of at least 8 characters and no more than 128 characters
+when I'm asked for character types to include in the password, I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters.
+WHEN I answer each prompt, my input will be validated and at least one character type should be selected.
+All prompts are answered and a password is generated that matches the selected criteria
+The password will be generated and is either displayed in an alert or written to the page!
--- script.js
+++ script.js
@@ -4,6 +4,32 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  let myPassword = "";
+  let passwordLength = parseInt (prompt ("Please enter the length of your password. It must be between 8 and 128 characters."))
+  if (passwordLength < 8 || passwordLength > 128){
+  alert ("password must be between 8 and 128 characters long")
+  // return ""
+  }
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
+console.log(randomCharacter,'char');
+var randomNumber = Math.floor((Math.random() * 10) + 1).toString();
+console.log(randomNumber,'num');
+// write a for loop
+for(let index = 0; index <= passwordLength; index++){
+  // construct myPassword using plus or concat
+  console.log(myPassword,"myPassword before")
+  myPassword.concat(randomCharacter);
+  console.log(myPassword,"myPassword  after")
+  myPassword.concat(randomNumber);
+  // if myPassword length is eqaul passwordLength
+  if(myPassword.length === passwordLength) {
+    // return myPassword
+    return myPassword;
+    }
+  }
+ }
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
