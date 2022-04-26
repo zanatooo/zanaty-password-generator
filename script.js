4b626cab5c76014bea9db930b89101ef7828f9dd
zanatooo <m8elzanaty@gmail.com>
2022-04-25 23:57:31 -0700

    fixed the loop

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   let myPassword = "";
-  let passwordLength = parseInt (prompt ("Please enter the length of your password. It must be between 8 and 128 characters."))
-  if (passwordLength < 8 || passwordLength > 128){
-  alert ("password must be between 8 and 128 characters long")
-  // return ""
+  let passwordLength = parseInt(prompt("Please enter the length of your password. It must be between 8 and 128 characters."))
+  if (passwordLength < 8 || passwordLength > 128) {
+    return "password must be between 8 and 128 characters long";
+    // return ""
   }
   let alphabet = ""
   const lower = "abcdefghijklmnopqrstuvwxyz";
@@ -17,10 +17,10 @@ function generatePassword() {
   const numbers = "0123456789"
   const special = "!@#$%&*"
 
-  var lowerCase = confirm ("Do you want lower case character?")
-  var upperCase = confirm ("Do you want uppercase character?")
-  var number = confirm ("Do you want a number?")
-  var specialCharecters = confirm ("Do you want special charecters?")
+  var lowerCase = confirm("Do you want lower case character?")
+  var upperCase = confirm("Do you want uppercase character?")
+  var number = confirm("Do you want a number?")
+  var specialCharecters = confirm("Do you want special charecters?")
 
   if (lowerCase === true) {
     alphabet += lower
@@ -34,25 +34,29 @@ function generatePassword() {
   if (specialCharecters === true) {
     alphabet += special
   }
-const randomCharacter = function() {
-  return alphabet.charAt(Math.floor(Math.random() * alphabet.length))
-}
-console.log(randomCharacter,'char');
-
-// write a for loop
-for(let index = 0; index <= passwordLength; index++){
-  // construct myPassword using plus or concat
-  console.log(myPassword,"myPassword before")
-  myPassword += randomCharacter();
-  console.log(myPassword,"myPassword  after")
-
-  // if myPassword length is eqaul passwordLength
-  if(myPassword.length === passwordLength) {
-    // return myPassword
-    return myPassword;
-    }
+  if(alphabet.length === 0){
+    return "Please choose minimum one option"
   }
- }
+  const randomCharacter = function () {
+    return alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+  }
+  console.log(randomCharacter, 'char');
+
+  // write a for loop
+  for (let index = 0; index < passwordLength; index++) {
+    // construct myPassword using plus or concat
+    console.log(myPassword, "myPassword before")
+    myPassword += randomCharacter();
+    console.log(myPassword, "myPassword  after")
+
+    // if myPassword length is eqaul passwordLength
+    // if(myPassword.length === passwordLength) {
+    // return myPassword
+    // return myPassword;
+    // }
+  }
+  return myPassword;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
